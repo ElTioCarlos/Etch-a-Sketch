f97f75505d16b6b2e968de3ea5d2e5d07dbee0d0
ElTioCarlosDeAlaska <alvarez2804lucas@gmail.com>
2025-02-06 20:04:56 -0300

    rainbow effect

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const container = document.querySelector(".container")
 let lastRow = 16
 
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function createPixel(rows) {    
     let cellSize = 550/rows
     
@@ -20,15 +24,15 @@ function delBoard() {
     pixels.forEach(pix => {pix.remove()})    
 }
 
-function clearBoard() {
-    
-}
-
 function pencil(size) {
     let cell = document.querySelectorAll(".pixel")
     cell.forEach(cel => {
         cel.addEventListener("mouseover", () => {
-            cel.setAttribute("style", `background-color: black; width: ${size}px; height: ${size}px`);
+            let colors = ["#ffadad","#ffd6a5","#fdffb6","#caffbf","#9bf6ff","#a0c4ff","#bdb2ff","#ffc6ff"]
+            let num = randomNumber(0,5)
+            let color = colors[num]
+            cel.setAttribute("style", `background-color: ${color}; width: ${size}px; height: ${size}px`);
+
 })})}
 
 document.querySelector("#size").addEventListener("click", () => {
@@ -45,10 +49,4 @@ document.querySelector("#clear").addEventListener("click", () => {
     let pixels = document.querySelectorAll(".pixel");
     pixels.forEach(pix => {
         pix.setAttribute("style", `background-color: white; width: ${size}px; height: ${size}px`);})
-})
-
-test = document.querySelector(".test")
-
-test.addEventListener("mouseover", () => {
-    test.setAttribute("style", "background-color: red;")
 })
